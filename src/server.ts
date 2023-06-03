1cb4b81056cd4d450cab1fa2ba5b47a7ba2df934
Thiago <thiagofreitascarneiro@hotmail.com>
2023-06-03 18:41:02 -0300

    creating middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastfy from 'fastify'
 import { env } from './env'
 import { transactionRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 
 const app = fastfy()
+
+app.register(cookie)
 
 app.register(transactionRoutes, {
   prefix: 'transactions',
